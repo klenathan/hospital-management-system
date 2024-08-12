e10e2b71754751fa09bfb0474f5b05065163221b
Nam Thai Tran <tran.nam.thai.02@gmail.com>
2024-08-12 17:46:41 +0700

    update: appointment & patients

--- backend/src/routes/appointments.route.ts
+++ backend/src/routes/appointments.route.ts
@@ -1,8 +1,17 @@
 import { Request, Response, Router } from "express";
+import AppointmentService from "../services/appointments.service";
 const appointmentRouter = Router();
 
-appointmentRouter.get("/", (_: Request, res: Response) => {
-  return res.send({ message: "GET appointments" });
+const appointmentService = new AppointmentService();
+appointmentRouter.get("/", async (_: Request, res: Response) => {
+  try {
+    const appoinemtns = await appointmentService.getAllAppointments();
+    return res.send(appoinemtns);
+  } catch (error) {
+    console.log(error);
+
+    return res.send(500);
+  }
 });
 
 export default appointmentRouter;
