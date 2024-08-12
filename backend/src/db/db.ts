67ffe67aa4031b3ad9f5f7dfad6f94521e03e961
Nam Thai Tran <tran.nam.thai.02@gmail.com>
2024-08-12 17:16:27 +0700

    feat: configBuilder

--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -8,6 +8,17 @@ const access: PoolOptions = {
   database: CONFIG.dbDatabase,
 };
 
+const configBuilder = (username: string, password: string) => {
+  return {
+    host: CONFIG.dbHost,
+    user: username,
+    password: password,
+    database: CONFIG.dbDatabase,
+  };
+};
+
+console.log(configBuilder("test", "test"));
+
 const connection = mysql.createConnection(access);
 //   .then((conn) => {
 //     console.log("Connected");
