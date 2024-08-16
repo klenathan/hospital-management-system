caa0818f5226c6348af12b82e1c3478d9ac7111e
Cong Phuong <s3804846@rmit.edu.vn>
2024-08-16 22:53:50 +0700

    Patient

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,22 @@
-export default function App() {
+
+import Login from "./Pages/Login";
+
+
+
+import Patient from "./Pages/Patient";
+import DataTable from "./Pages/DataTable"; 
+
+function App() {
   return (
-    <div>
-      <div></div>
-    </div>
+    <div className="flex">
+    <Patient />
+    <main className="flex-1 bg-gray-100 min-h-screen">
+        <DataTable />
+      </main>
+    
+  </div>
   )
 }
+
+
+export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,34 +1,10 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import { UserProvider } from './hooks/Auth/UserContext'
-import ErrorPage from './layouts/ErrorPage'
-import LandingPage from './layouts/LandingPage'
-import './styles/index.css'
-const queryClient = new QueryClient()
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-
-    children: [
-      {
-        path: '/djtmecuocdoi',
-        element: <div className='bg-red-300 w-screen h-screen'>Hello</div>
-      },
-      {
-        path: '',
-        element: <LandingPage />
-      }
-    ],
-    errorElement: <ErrorPage />
-  }
-])
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <UserProvider>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
-  </UserProvider>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
