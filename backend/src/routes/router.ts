98535171e70f17890e125343ad0d00acf0a67154
Nam Thai Tran <tran.nam.thai.02@gmail.com>
2024-08-21 19:06:06 +0700

    Feat: Mongo blobs

--- backend/src/routes/router.ts
+++ backend/src/routes/router.ts
@@ -7,6 +7,7 @@ import authRouter from "./auth.route";
 import treatmentRouter from "./treatments.route";
 import departmentRouter from "./departments.route";
 import protectedRouteMiddleware from "../middlewares/protectedRoutes";
+import blobRouter from "./blobs.route";
 
 const router = Router();
 
@@ -15,6 +16,22 @@ router.use(
   authRouter
   /* 
     #swagger.tags = ['Auth']
+
+    #swagger.responses[500] = {
+        schema: { $ref: '#/definitions/serverErrSchema' }
+    }  
+
+    #swagger.responses[400] = {
+        schema: { $ref: '#/definitions/errSchema' }
+    }
+    */
+);
+router.use(
+  "/blob",
+  protectedRouteMiddleware,
+  blobRouter
+  /* 
+    #swagger.tags = ['Blobs']
 
     #swagger.responses[500] = {
         schema: { $ref: '#/definitions/serverErrSchema' }
